1ede9d078ee7ea68c371ecc36498570c6c41f2fc
Paul Frischknecht <paulfrischknechtmx@gmail.com>
2022-05-06 11:55:34 +0200

    try mutator groups

--- src/test/java/ch/paulfrischknecht/MainTest.java
+++ src/test/java/ch/paulfrischknecht/MainTest.java
@@ -5,10 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
-    @Test
+    /*@Test
     void t1() {
         assertDoesNotThrow(
             () -> Main.main()
         );
-    }
+    }*/
 }
--- test.sh
+++ test.sh
@@ -3,11 +3,16 @@ set -euo pipefail
 
 #(
 
+# looks like explicit clean is necessary, otherwise jacoco does not fail on too little coverage
+mvn clean
+
+#rm -rf target/site/jacoco
 echo "Normal test run..."
 mvn test jacoco:report@report jacoco:check@check
 echo "Coverage goal met, see report:"
 ls -l target/site/jacoco/index.html
 
+#rm -rf target/pit-reports
 echo "Mutation test run..."
 mvn test-compile org.pitest:pitest-maven:mutationCoverage
 echo "Mutation Coverage goal met, see report:"
