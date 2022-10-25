4536d4b3f8cd46e5638b8fece5c491c2758ec82a
Paul Frischknecht <paulfrischknecht@paulfrischknecht.ch>
2022-10-25 21:17:13 +0200

    i

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-where mvn || sudo apt-get install -y maven
+command -v mvn || sudo apt-get install -y maven
 # also tested with jdk Java 18
-where java javac javap || sudo apt-get install -y openjdk-11-jdk
+command -v java javac javap || sudo apt-get install -y openjdk-11-jdk
